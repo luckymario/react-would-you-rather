5eb6126e60498df4b7dedc58864ab6e9c50a7492
Mario <kowalewski.mariusz@gmail.com>
2018-09-18 22:30:54 +0200

    feat: Add vote to questions array

--- src/reducers/questions.js
+++ src/reducers/questions.js
@@ -8,14 +8,68 @@ export default function questions (state = {}, action) {
 				...action.questions
 			}
 
-		case SAVE_ANSWER :
+		/*case TOGGLE_TWEET :
+			// data: _saveLikeToggle ({ id, hasLiked, authedUser })
+			tweets = {
+        ...tweets,
+        [id]: {
+          ...tweets[id],
+          likes: hasLiked === true
+            ? tweets[id].likes.filter((uid) => uid !== authedUser)
+            : tweets[id].likes.concat([authedUser])
+        }
+      }
+
+			// reducer
 			return {
 				...state,
+				[action.id]: {
+					...state[action.id],
+					likes: action.hasLiked === true
+						? state[action.id].likes.filter((uid) => uid !== action.authedUser)
+						: state[action.id].likes.concat([action.authedUser])
+				}
+			}	*/
+
+		case SAVE_ANSWER :
+			/*_saveQuestionAnswer ({ authedUser, qid, answer }):
+			users = {
+        ...users,
+        [authedUser]: {
+          ...users[authedUser],
+          answers: {
+            ...users[authedUser].answers,
+            [qid]: answer
+          }
+        }
+      }
+
+      questions = {
+        ...questions,
+        [qid]: {
+          ...questions[qid],
+          [answer]: {
+            ...questions[qid][answer],
+            votes: questions[qid][answer].votes.concat([authedUser])
+          }
+        }
+      } */
+
+			return {
+				/*...state,
 				[action.authedUser]: {
           ...state[action.authedUser],
           answers: {
             ...state[action.authedUser].answers,
             [action.qid]: action.answer
+          }
+        }*/
+        ...state,
+        [action.qid]: {
+          ...state[action.qid],
+          [action.answer]: {
+            ...state[action.qid][action.answer],
+            votes: state[action.qid][action.answer].votes.concat([action.authedUser])
           }
         }
 			}
